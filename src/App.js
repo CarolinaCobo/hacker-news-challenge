b1f283aeed194e1f14b412a816c53aae1dc2484d
Carolina Cobo <ccobo.dev@gmail.com>
2021-08-27 20:16:28 +0100

    Fetch object by their ID from the Items API

--- src/App.js
+++ src/App.js
@@ -9,8 +9,19 @@ export default function App() {
         "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
       );
       const jsonData = await res.json();
-      console.log(jsonData);
-      return jsonData;
+
+      const currentPosts = jsonData.slice(0, 10);
+      const posts = await Promise.all(
+        currentPosts.map(async (id) => {
+          const res = await fetch(
+            `https://hacker-news.firebaseio.com/v0/item/${id}.json?print=pretty`
+          );
+          const jsonData = await res.json();
+          return jsonData;
+        })
+      );
+      setData(posts);
+      console.log(posts);
     };
 
     getData();
