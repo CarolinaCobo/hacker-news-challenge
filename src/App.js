0ea12f8d3062fcc14bf7b0cf9faac0fcaf45a52e
Carolina Cobo <ccobo.dev@gmail.com>
2021-08-28 12:18:17 +0100

    Add loading message when fetching data

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import { useState, useEffect } from "react";
 export default function App() {
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(true);
 
+  // Fetch API to get all the ID numbers of the top stories
   useEffect(() => {
     const getData = async () => {
       const res = await fetch(
@@ -18,6 +20,7 @@ export default function App() {
         endOfPageNum
       );
 
+      //   Fetch data from API with all the details using the ID
       const posts = await Promise.all(
         currentPosts.map(async (id) => {
           const res = await fetch(
@@ -28,7 +31,7 @@ export default function App() {
         })
       );
       setData(posts);
-      console.log(posts);
+      setLoading(false);
     };
 
     getData();
@@ -45,6 +48,11 @@ export default function App() {
 
   return (
     <div className="App">
+      {loading ? <div>Loading</div> : null}
+      {loading === false
+        ? data.map((post) => <div key={post.id}>{post.title}</div>)
+        : null}
+
       <div>
         <div>Page {currentPage} </div>
         <button onClick={handlePrev}>Previous</button>
@@ -53,3 +61,10 @@ export default function App() {
     </div>
   );
 }
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById("root")
+);
